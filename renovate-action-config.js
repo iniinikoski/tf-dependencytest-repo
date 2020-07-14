64dab43b124230225da1836744232d40be978b48
Iiro Niinikoski <iiro.niinikoski@redbull.com>
2020-07-14 19:43:36 +0200

    Trying domainName.

--- renovate-action-config.js
+++ renovate-action-config.js
@@ -8,10 +8,13 @@ module.exports = {
   repositories: [
     'iniinikoski/tf-dependencytest-repo',
   ],
-  "hostRules": [{
-    "hostName": "app.terraform.io",
-    "encrypted": {
-      "token": "jDplnkaJhNRGnqLd9opzhmBLSJl7XR3ogIT4TsCyEuYhWJKLqrWASdCaOBvZkJEe6R6MXJTPFOuZv9HTbHz2QnU4a0VBHFZG70r3MNnFY0h5iCslQujfC866RFNouoOKAQujR/i8FtFIsgZKTLQAcxnOH7ce6EAF4fCXAsrwxiWI5VfuZ7k5uW03sCM/+LSLAYof4CduPz5gy6RWd/oR9pqZAGusxuk/k4yd+hHdE2rCxQL/ZotcjhpGeFUqf//cyt8b1DgODoLhusGTHrAsT0B0DdhsU5xJLFY85JokFZX8Fl36ReXpbbSlA3YWrGSiGIzCRwoyNMW5YKHuGDP2vg=="
+  "hostRules": [
+    {
+      "domainName": "terraform.io",
+      "encrypted": {
+        "token": "jDplnkaJhNRGnqLd9opzhmBLSJl7XR3ogIT4TsCyEuYhWJKLqrWASdCaOBvZkJEe6R6MXJTPFOuZv9HTbHz2QnU4a0VBHFZG70r3MNnFY0h5iCslQujfC866RFNouoOKAQujR/i8FtFIsgZKTLQAcxnOH7ce6EAF4fCXAsrwxiWI5VfuZ7k5uW03sCM/+LSLAYof4CduPz5gy6RWd/oR9pqZAGusxuk/k4yd+hHdE2rCxQL/ZotcjhpGeFUqf//cyt8b1DgODoLhusGTHrAsT0B0DdhsU5xJLFY85JokFZX8Fl36ReXpbbSlA3YWrGSiGIzCRwoyNMW5YKHuGDP2vg=="
+      },
+      "abortOnError": true
     }
-  }]
+  ]
 };
