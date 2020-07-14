153baae2db3b3b50aa99ed35b1899e0e524ee74f
Iiro Niinikoski <iiro.niinikoski@redbull.com>
2020-07-15 00:17:34 +0200

    Moving the hostRules back to config.

--- renovate-action-config.js
+++ renovate-action-config.js
@@ -1,11 +1,27 @@
+const terraformAccessToken = process.env.TF_ACCESS_TOKEN;
+
+if(! terraformAccessToken){
+  console.log("The TF_ACCESS_TOKEN environment variable must be set")
+  process.exit(1);
+}
+
 module.exports = {
   branchPrefix: 'test-renovate-action/',
   dryRun: true,
-  logLevel: 'trace',
+  // logLevel: 'trace',
+  logLevel: 'debug',
   onboarding: false,
   platform: 'github',
+  enabledManagers: ["terraform"]
   includeForks: false,
   repositories: [
     'iniinikoski/tf-dependencytest-repo',
   ],
+  "hostRules": [
+    {
+      "hostName": "app.terraform.io",
+      "token": terraformAccessToken,
+      "abortOnError": true
+    }
+  ]
 };
